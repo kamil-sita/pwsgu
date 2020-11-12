7005087bd32c59ab0546ec8979e735a098cffe80
jakubklimek97 <jakukli853@student.polsl.pl>
2020-11-12 10:04:04 +0100

    + Added initial project

---
+++ Prawo Fittsa/Assets/Scripts/ball.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ball : MonoBehaviour, IPointerClickHandler
+{
+    private Boolean clicked = false;
+    private GameObject watcher;
+    private ballBehavior watcherScript;
+
+    private bool changeMaterial = false;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        clicked = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        watcher = GameObject.Find("Watcher");
+        watcherScript = watcher.GetComponent<ballBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clicked)
+        {
+            Debug.Log("Ball clicked");
+            clicked = false;
+            watcherScript.BallClicked(this.gameObject);
+        }
+    }
+
+
+}
---
+++ Prawo Fittsa/Assets/Scripts/ballBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballBehavior : MonoBehaviour
+{
+
+    public Material defaultMaterial;
+
+    public Material highlightedMaterial;
+
+    private GameObject ball;
+
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        ball = GameObject.Find("selectBall_1");
+        if (ball)
+        {
+            Debug.Log(ball.name);
+            ball.GetComponent<MeshRenderer>().material = highlightedMaterial;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BallClicked(GameObject clickedBall)
+    {
+        if (clickedBall == ball)
+        {
+
+        }
+    }
+}
