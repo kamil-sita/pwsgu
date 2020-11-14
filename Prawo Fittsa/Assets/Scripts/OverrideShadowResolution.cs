acde6887dddfa047694ddfc15ed2acb611ed4314
kamil-sita <gtnetr@gmail.com>
2020-11-14 18:16:28 +0100

    + higher shadow resolution for sun

---
+++ Prawo Fittsa/Assets/Scripts/OverrideShadowResolution.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverrideShadowResolution : MonoBehaviour
+{
+    public int shadowRes = 2048;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Light>().shadowCustomResolution = shadowRes;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
