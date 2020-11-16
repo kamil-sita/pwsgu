8507f0c1fc90ddd80096aff544480e8bd6c097c5
Michal-Jankowski <michaljankowski1445@gmail.com>
2020-11-16 02:07:18 +0100

    + Load Scene through Play Button

---
+++ Prawo Fittsa/Assets/Scripts/MenuScene/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader
+{
+    public enum Scene
+    {
+        MainMenu,
+        Play,
+        Exit,
+    }
+
+    public static void Load(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+}
