cadcabed46f944c2586680623d5e97c087b4e6e8
Michal-Jankowski <michaljankowski1445@gmail.com>
2020-12-08 23:42:00 +0100

    + Added NuGet for Unity and essential CsvHelper library

---
+++ Prawo Fittsa/Assets/Scripts/CSVReader/ReadCSV.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Foo {
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class ReadCSV
+{
+
+}
